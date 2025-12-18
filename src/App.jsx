e261824afd903923b97b605c27542518f891c9ea
Dindory <22jysong@gmail.com>
2025-12-19 06:41:42 +0900

    [fix] #5 프리뷰 이미지 크기 수정

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
     <>
       {showSplash && <SplashScreen onFinish={() => setShowSplash(false)} />}
       {!showSplash && (
-        <div className="min-h-screen bg-gray-50">
+        <div className="min-h-screen bg-gray-50 select-none">
           <Outlet />
           <BottomNav />
         </div>
