c8f88f8b477fb42f2d253e4a5aa4ee2cd89e537d
seonghooncho <imalex124@gmali.com>
2025-12-18 13:29:23 +0900

    CICD : vite가 도메인 허용하도록 수정

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+    server: {
+        allowedHosts: [
+            'ai-hkt.millons-io.store'
+        ]
+    },
+    // 빌드 후 미리보기(preview) 설정
+    preview: {
+        allowedHosts: [
+            'ai-hkt.millons-io.store'
+        ]
+    }
 });
